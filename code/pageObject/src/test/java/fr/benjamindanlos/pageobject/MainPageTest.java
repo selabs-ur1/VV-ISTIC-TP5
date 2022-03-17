27af1eeff291ea58b1657d36256ad62de32a11d7
Danlos <benjamin.danlos@etudiant.univ-rennes1.fr>
2022-03-17 14:47:18 +0100

    find bug

---
+++ code/pageObject/src/test/java/fr/benjamindanlos/pageobject/MainPageTest.java
@@ -0,0 +1,40 @@
+package fr.benjamindanlos.pageobject;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static com.codeborne.selenide.Condition.attribute;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.*;
+
+public class MainPageTest {
+    MainPage mainPage = new MainPage();
+
+    @BeforeAll
+    public static void setUpAll() {
+        Configuration.browserSize = "1280x800";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @BeforeEach
+    public void setUp() {
+        open("http://localhost:4200/create");
+    }
+
+    /**
+     * User should not be able to click "next" when fields are not set
+     */
+    @Test
+    public void testNextStepDisabled() {
+        mainPage.step2Button.click();
+
+        mainPage.nextButton.shouldBe(Condition.disabled);
+    }
+
+}
